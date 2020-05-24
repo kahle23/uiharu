9a3799112766a71f83547b97afcb41372faee496
Kahle <kahlkn@gmail.com>
2020-05-24 22:11:48 +0800

    Create project

---
+++ src/main/java/uiharu/Application.java
@@ -0,0 +1,28 @@
+package uiharu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Startup class.
+ * @author Kahle
+ */
+@EnableAsync
+@EnableScheduling
+@SpringBootApplication
+public class Application {
+    private static Logger log = LoggerFactory.getLogger(Application.class);
+
+    /**
+     * Program entry.
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        log.info("Uiharu start success. ");
+    }
+
+}
